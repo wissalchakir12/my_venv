1eaada7baa4df71fdac71c1347eac7865c8a0375
wissal <125746468+wissalchakir12@users.noreply.github.com>
2023-05-18 21:47:24 +0200

    first commit

---
+++ e_commerce/store/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path
+
+from . import views
+
+urlpatterns = [
+        #Leave as empty string for base url
+	path('store/', views.store, name="store"),
+	path('cart/', views.cart, name="cart"),
+	path('checkout/', views.checkout, name="checkout"),
+    path('', views.Home, name="Home"),
+    
+    
+
+]
